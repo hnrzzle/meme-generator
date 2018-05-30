01f585546581e7ed29bbcf6e2e8b83d642a5d5c6
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-05-29 17:32:38 -0700

    able to change title!

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
+import dom2image from 'dom-to-image';
+import fileSaver from 'file-saver';
 
 export default class App extends Component {
 
   constructor() {
     super();
+
+    this.state = {
+      title: 'Insert Headline Here',
+      image: null
+    };
+  }
+
+  handleTitleChange({ target }) {
+    this.setState({ title: target.value });
+
   }
 
   render() {
+    const { title, image } = this.state;
+
     return (
       <main>
-        <h1>Hey</h1>
+        <h1>Meme Generator</h1>
+        <fieldset>
+          <div>
+            <label>
+              Text:
+              <input
+                value={title}
+                onChange={event => this.handleTitleChange(event)}
+              />
+            </label>
+          </div>
+        </fieldset>
   
       </main>
     );
