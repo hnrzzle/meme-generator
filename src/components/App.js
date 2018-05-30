5949a842d3ece3c91273bbcc3c8d1a244789f3d6
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-05-29 17:02:40 -0700

    start indexjs

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
-console.log('hello!');
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <main>
+        <h1>Hey</h1>
+  
+      </main>
+    );
+
+
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import css from './main.css';
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
